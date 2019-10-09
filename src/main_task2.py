b4cca184849304a050a8ca26609bc1bf6e719bad
Bobrov Ivan <id.bobr@gmail.com>
2019-10-09 13:47:40 +0300

    Dichotomy implemented

--- src/main_task2.py
+++ src/main_task2.py
@@ -1,5 +1,6 @@
 import math
 from task2.exhaustive_search import ExhaustiveSearch
+from task2.dichotomy import Dichotomy
 
 
 def cubic_function(x):
@@ -14,19 +15,19 @@ def sin_function(x):
     return x * math.sin(1/x)
 
 
-for optimization_method in ([ExhaustiveSearch()]):
+for optimization_method in ([ExhaustiveSearch(), Dichotomy()]):
     result = optimization_method.find_minimum(cubic_function, 0, 1, 0.001)
     print(optimization_method.get_name() + " | cubic function: " + str(result.x_minimum) +
           "; iterations: " + str(result.iterations_number) +
           "; function calls: " + str(result.function_calls))
 
-for optimization_method in ([ExhaustiveSearch()]):
+for optimization_method in ([ExhaustiveSearch(), Dichotomy()]):
     result = optimization_method.find_minimum(abs_function, 0, 1, 0.001)
     print(optimization_method.get_name() + " | abs function: " + str(result.x_minimum) +
           "; iterations: " + str(result.iterations_number) +
           "; function calls: " + str(result.function_calls))
 
-for optimization_method in ([ExhaustiveSearch()]):
+for optimization_method in ([ExhaustiveSearch(), Dichotomy()]):
     result = optimization_method.find_minimum(sin_function, 0.1, 1, 0.001)
     print(optimization_method.get_name() + " | sin function: " + str(result.x_minimum) +
           "; iterations: " + str(result.iterations_number) +
